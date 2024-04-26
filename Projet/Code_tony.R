43383373fc17fd5b9847098e14cacfdd19c1904b
tlauze <tony.lauze@ensae.fr>
2024-04-26 15:36:00 +0000

    fin code tony + essai markdown

--- Projet/Code_tony.R
+++ Projet/Code_tony.R
@@ -33,6 +33,7 @@ df_corr <- df_corr[order(df_corr$Date), ]
 df_brut$Prod_water <- as.numeric(df_brut$Prod_water)
 df_corr$Prod_water <- as.numeric(df_corr$Prod_water)
 
+
 ### Visualisation ###
 
 #Figure 1
@@ -67,16 +68,20 @@ plot(data_diff$index, data_diff$diff_prod_water, type = "l",
 #### STATIONNARITE ####
 
 # Calculer la série différenciée
-diff_prod_water <- diff(df_corr$Prod_water)
+diff_prod_water <- diff(df_corr$Prod_water) %>% ts()
+diff_prod_water
 
 # Tracer la série différenciée
+par(mfrow = c(1, 1))
 plot(diff_prod_water, type = "l", 
      xlab = "Time index", ylab = "", 
      main = "Differentiated Series")
 
+
 ### TESTS ###
 
 ## Before making unit root test we test that there is not a linear trend
+
 # Créer un objet data.frame avec la série différenciée
 data_diff <- data.frame(diff_prod_water = diff_prod_water)
 
@@ -90,23 +95,22 @@ lm_result_diff <- lm(diff_prod_water ~ index, data = data_diff)
 summary(lm_result_diff)
 
 #EXPORTER LES RESULTATS
-install.packages("stargazer")
 library(stargazer)
-
 stargazer(lm_result_diff, title="Simple regression results",
           single.row=TRUE, no.space=TRUE)
 
 
-#package pour des tests plus modulables
-install.packages("fUnitRoots")
+## TESTS ADF ##
 library(fUnitRoots)
 
-## TESTS ADF ##
-
-adf <- adfTest(diff_prod_water, lag=0, type="nc") #
-
+#exemple d'utilisation du test ADF
+adf <- adfTest(diff_prod_water, lag=1, type="nc") #on prend "nc" car il n'y a ni tendance, ni niveau fixe
 adf
 
+#Comment choisir le lag ? il faut prendre le plus petit qui "marche", 
+#c'est à dire le plus petit pour lequel on n'a plus d'autocorréltion des résidus du modèle
+
+#fonction qui teste l'autocorrélation de résidus jusqu'à l'ordre k
 Qtests <- function(series, k, fitdf=0) {
   pvals <- apply(matrix(1:k), 1, FUN=function(l) {
     pval <- if (l<=fitdf) NA else Box.test(series, lag=l, type="Ljung-Box", fitdf=fitdf)$p.value
@@ -117,7 +121,10 @@ Qtests <- function(series, k, fitdf=0) {
 
 Qtests(adf@test$lm$residuals, 24, fitdf = length(adf@test$lm$coefficients))
 
-series <- diff_prod_water; kmax <- 24; adftype="nc"
+series <- diff_prod_water
+kmax <- 24
+adftype="nc"
+
 adfTest_valid <- function(series, kmax, adftype){
   k <- 0
   noautocorr <- 0
@@ -134,18 +141,24 @@ adfTest_valid <- function(series, kmax, adftype){
 }
 
 adf <- adfTest_valid(diff_prod_water,24,adftype="nc")
+adf
 
 #test ADF final avec lag=7
-adf <- adfTest(diff_prod_water, lag=7, type="nc") #
+adf <- adfTest(diff_prod_water, lag=7, type="nc")
 adf
 
 ## TEST KPSS ##
-install.packages("tseries")
+
+#Ce test a pour hypothèse nulle la stationnarité de la série, donc on ne veut pas pouvoir rejeter H0
+#Ce test est complémnetaire du test ADF
+
 library("tseries")
 ?kpss.test
 
 kpss_result <- kpss.test(diff_prod_water, null="Level")
 kpss_result
+
+
 
 #### MODELISATION ####
 
@@ -154,10 +167,10 @@ kpss_result
 par(mfrow = c(2, 1), mar = c(2, 4, 4, 1))  # Diviser la fenêtre graphique en 2 lignes et 1 colonne
 
 # Tracer l'ACF
-acf(diff_prod_water, main = "Fonction d'autocorrélation (ACF)")
+acf(diff_prod_water, main = "Autocorrelation function (ACF)")
 
 # Tracer le PACF
-pacf(diff_prod_water, main = "Fonction d'autocorrélation partielle (PACF)")
+pacf(diff_prod_water, main = "Partial autocorrelation function (PACF)")
 
 
 #### ESTIMATION ####
@@ -170,11 +183,12 @@ library('astsa')
 arma_model <- sarima(diff_prod_water, 7, 0, 1, details=FALSE, no.constant = TRUE)
 arma_model$ttable
 
-#test portemanteau
+#test portemanteau ?
 install.packages('portes')
 library('portes')
 LjungBox(arma_model$fit, lags=seq(1,24,1))
 ?LjungBox
+
 
 ## MODEL SELECTION
 
@@ -192,13 +206,28 @@ for (p in 1:7) {
 results
 
 
+# Résidus gaussiens du modèle ?
 
+arma_model <- sarima(diff_prod_water, 7, 0, 1, details=FALSE, no.constant = TRUE)
 
+?checkresiduals
+checkresiduals(arma_model$fit, lag=24)
 
+autoplot(arma_model$fit)
 
+##### PREVISION ######
 
+prod_water = ts(df_corr$Prod_water, start = c(1990, 1), end = c(2024, 2), frequency = 12)
+prod_water
 
+?arima
+arima_model <- arima(prod_water, order=c(7, 1, 1), include.mean = FALSE)
+arima_model
 
+forecast_res <- forecast(arima_model, level=c(95), h=2)
+forecast_res
+
+plot(forecast_res, include=50)
 
 
 
